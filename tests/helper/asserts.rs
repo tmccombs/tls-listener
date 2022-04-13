330e6bcec4784384d0a77f60f3eac8d065326683
Thayne McCombs <astrothayne@gmail.com>
2022-04-13 01:25:09 -0600

    Start adding integration tests

---
+++ tests/helper/asserts.rs
@@ -0,0 +1,21 @@
+macro_rules! assert_err {
+    ($ex:expr, $m:pat) => {
+        match $ex {
+            Err($m) => (),
+            Err(e) => panic!("Unexpected error: {:?}", e),
+            Ok(_) => panic!("Expected error, but got Ok"),
+        }
+    };
+}
+
+macro_rules! assert_ascii_eq {
+    ($one:expr, $two:expr) => {
+        assert_eq!(
+            ::std::str::from_utf8(&*$one).unwrap(),
+            ::std::str::from_utf8(&*$two).unwrap()
+        )
+    };
+}
+
+pub(crate) use assert_ascii_eq;
+pub(crate) use assert_err;
---
+++ tests/helper/mod.rs
@@ -0,0 +1,27 @@
+use futures_util::StreamExt;
+use tls_listener::TlsListener;
+use tokio::io::{copy, split};
+use tokio::task::JoinHandle;
+
+mod asserts;
+pub(crate) use asserts::*;
+
+mod mocks;
+pub use mocks::*;
+
+pub fn setup() -> (MockConnect, TlsListener<MockAccept, MockTls>) {
+    let (connect, accept) = accepting();
+    (connect, TlsListener::new(MockTls, accept))
+}
+
+pub fn setup_echo() -> (MockConnect, JoinHandle<()>) {
+    let (connector, listener) = setup();
+
+    let handle = tokio::spawn(listener.for_each_concurrent(None, |s| async {
+        let (mut reader, mut writer) = split(s.expect("Unexpected error"));
+        copy(&mut reader, &mut writer)
+            .await
+            .expect("Failed to copy");
+    }));
+    (connector, handle)
+}
